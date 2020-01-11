d2597ce1e7685e6884f4eb1901fe1a90d8e7937e
Rand al'Thor <alexanderhogle@gmail.com>
2020-01-10 21:21:48 -0600

    adds values to dep, role, and employee tables

--- seed.sql
+++ seed.sql
@@ -24,10 +24,18 @@ CREATE TABLE employee (
     role_id INT,
     manager_id INT NULL,
     PRIMARY KEY (id)
-)
+);
 
 
 INSERT INTO department (name)
 VALUES ("Sales"), ("Financial"), ("Shipping"), ("Personal Relations");
 
+INSERT INTO role (title, salary)
+VALUES ("Manager", 10000), ("Intern", 1000), ("Junior Employee", 6000), ("Senior Employee", 8000);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Alexander", "Hogle", 2, NULL), ("Ronald", "Pfiester", 1, NULL), ("Cameron", "Gideon", 4, NULL), ("Cameron", "Johnston", 3, NULL), ("Timothy", "Greenlee", 1, NULL);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
