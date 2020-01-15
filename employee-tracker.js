d6584a38e0f2b2b48e7ebde76bd7cf0bf29977fa
Rand al'Thor <alexanderhogle@gmail.com>
2020-01-14 19:47:29 -0600

    connects to mysql

--- employee-tracker.js
+++ employee-tracker.js
@@ -2,3 +2,33 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var cTable = require("console.table");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    //Port
+    port: 3306,
+
+    // Username
+    user: "root",
+
+    //Password
+    password: "",
+    database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    runSearch();
+});
+
+function runSearch() {
+    inquirer
+        .prompt({
+
+        })
+        .then(function(answer) {
+            switch (answer.action) {
+            case
+            }
+        });
+}
