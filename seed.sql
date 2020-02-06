8f995b9fe8b6bb15a06b609d385779a7a6f3191a
Rand al'Thor <alexanderhogle@gmail.com>
2020-02-06 13:58:14 -0600

    adds roles with respective department ids

--- seed.sql
+++ seed.sql
@@ -1,8 +1,8 @@
 INSERT INTO department (name)
-VALUES ("Sales"), ("Financial"), ("Shipping"), ("Personal Relations");
+VALUES ("Sales"), ("Accounting"), ("Shipping"), ("Human Resources");
 
 INSERT INTO role (title, salary)
-VALUES ("Manager", 10000), ("Intern", 1000), ("Junior Employee", 6000), ("Senior Employee", 8000);
+VALUES ("Sales Manager", 10000, 1), ("Junior Salesperson", 4000, 1), ("Account Director", 110000, 2), ("Senior Accountant", 85000, 2), ("Head of Shipping", 75000, 3), ("Head of Receiving", 75000, 3), ("HR Manager", 120000, 4), ("Benefits Manager", 115000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Alexander", "Hogle", 2, NULL), ("Ronald", "Pfiester", 1, NULL), ("Cameron", "Gideon", 4, NULL), ("Cameron", "Johnston", 3, NULL), ("Timothy", "Greenlee", 1, NULL);
