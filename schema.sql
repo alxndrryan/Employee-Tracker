fd7063c020e88e5275c06899b38c04951393e100
Rand al'Thor <alexanderhogle@gmail.com>
2020-02-06 13:37:05 -0600

    fixes tables and gives schema its own file

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6,2) NOT NULL,
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL
+);
