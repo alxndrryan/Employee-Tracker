43b9c850335b36a0e700cbc9584a8d8a6f42f123
Rand al'Thor <alexanderhogle@gmail.com>
2020-02-07 21:23:57 -0600

    adds add department and role functions

--- app.js
+++ app.js
@@ -11,27 +11,7 @@ const db = new Database({
     database: "employee_trackerDB"
 });
 
-//Add departments, roles, employees
-
-//View departments, roles, employees
-
-async function viewRoles() {
-    await db.query('', (err, res) => {
-        if (err) throw err;
-        console.log(' ');
-        console.table(res);
-
-    })
-};
-
-async function viewRoleSummary() {
-    console.log(' ');
-    await db.query('SELECT r.id, title, salary, name AS department FROM role r LEFT JOIN department d ON department_id = d.id', (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        
-    })
-};
+//VIEW departments, roles, employees
 
 //View departments
 
@@ -45,6 +25,88 @@ async function viewDepartments() {
     })
 };
 
+//View roles
+async function viewRoles() {
+    console.log(' ');
+    await db.query('SELECT r.id, title, salary, name AS department FROM role r LEFT JOIN department d ON department_id = d.id', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        
+    })
+};
+
+//view employees
+async function viewEmployees() {
+    console.log(' ');
+    await db.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+
+    });
+};
+
+//ADD departments, roles, employees
+
+//add department
+async function addDepartment() {
+    inquirer.prompt([
+        {
+            name: "deptName",
+            type: "input",
+            message: "Enter new department:",
+            validate: async function confirmStringInput(input) {
+                if ((input.trim() != "") && (input.trim().length <= 30)) {
+                    return true;
+                }
+                return "Invalid input. Please limit your input to 30 characters or less."
+            }
+        }
+    ]).then(answer => {
+        db.query("INSERT INTO department (name) VALUES (?)", [answer.deptName]);
+        console.log("\x1b[32m", `${answer.deptName} was added to departments.`);
+    })
+};
+
+//add roles
+async function addRole() {
+    let dept = await db.query('SELECT id, name FROM department');
+
+    inquirer.prompt([
+        {
+            name: "rName",
+            type: "input",
+            message: "Enter new role title:",
+            validate: async function confirmStringInput(input) {
+                if ((input.trim() != "") && (input.trim().length <= 30)) {
+                    return true;
+                }
+                return "Invalid input. Please limit your input to 30 characters or less."
+            }
+        },
+        {
+            name: "salNum",
+            type: "input",
+            message: "Enter role salary:",
+            validate: input => {
+                if (!isNaN(input)) {
+                    return true;
+                }
+                return "Enter a valid number."
+            }
+        },
+        {
+            name: "roleDept",
+            type: "list",
+            message: "Choose role department:",
+            choices: dept.map(obj => obj.name)
+        }
+    ]).then(answer => {
+        let deptID = dept.find(obj => obj.name === answer.roleDept).id
+        db.query("INSERT INTO role (title, salary, department_id) VALUES (?)", [[answer.rName, answer.salNum, deptID]]);
+        console.log("\x1b[32m", `${answer.rName} was added to the ${answer.roleDept} department.`);
+    })
+}
+
 //Update employee roles
 
 //BONUS
@@ -54,8 +116,10 @@ async function viewDepartments() {
 //View total utilized budget of a department --ie the combined salaries of all employees in that department
 
 function init() {
-    viewDepartments();
-    viewRoleSummary();
+    // viewDepartments();
+    // viewRoles();
+    // viewEmployees();
+    addRole();
 }
 
 
